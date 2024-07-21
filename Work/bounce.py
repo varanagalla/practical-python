26ad688b4551c2d9dee9ba355c71ebde2c5b0bb8
Aarav Prasad <aarav.prasad983@gmail.com>
2024-07-21 12:01:05 -0700

    Added some changes.

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,19 @@
 # bounce.py
 #
 # Exercise 1.5
+
+"""
+A rubber ball is dropped from a height of 100 meters and each time it hits the ground,
+it bounces back up to 3/5 the height it fell. Write a program bounce.py that prints
+a table showing the height of the first 10 bounces.
+"""
+
+fell_from_height = 100 # meters
+bounce_back_fraction = 3/5
+no_of_bounces = 10
+
+while no_of_bounces > 0:
+    fell_from_height *= bounce_back_fraction
+    fell_from_height = round(fell_from_height, ndigits = 4)
+    print(no_of_bounces, fell_from_height)
+    no_of_bounces -= 1
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,30 @@
 # mortgage.py
 #
 # Exercise 1.7
+"""
+Dave has decided to take out 30-year fixed rate mortgage of $500,000 with Guido's mortgage.
+Interest rate is 5% and the monthly payment is $2684.11
+Calculate the total amount that Dave will have to pay over the life of the mortgage.
+
+"""
+principal = 500000.0
+rate = 0.05
+monthly_payment = 2684.11
+total_paid = 0.0
+months_paid = 0
+extra_payment = 1000.0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+
+while principal > 0:
+    principal = principal * (1+rate/12) - monthly_payment
+    total_paid += monthly_payment
+    months_paid += 1
+    if months_paid >= extra_payment_start_month and months_paid <= extra_payment_end_month:
+        principal -= extra_payment
+        total_paid += extra_payment
+    total_paid += min(principal, 0)
+    principal -= min(principal, 0)
+    print(f"{months_paid:>3d} {total_paid:=10.2f} {principal:>10.2f}")
+
+print(f"Total paid: {round(total_paid,2)} over {months_paid} months.")
